32c41540da09892fc647d81cb894ca7b3633a3eb
Anna Jablonska <anna@senken.io>
2023-06-20 16:40:53 +0200

    new app in react and jsx

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import './App.css';
+import Grid from './Grid'
+import Sidebar from './Sidebar';
+
+function App() {
+
+
+  return (
+    
+    <div className="App">
+      <Grid />
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
