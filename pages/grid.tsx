e4c0ccb05e047dd128242152dd360f42763899f2
Anna Jablonska <anna@senken.io>
2023-08-28 17:20:32 +0200

    change border for items on grid

--- pages/grid.tsx
+++ pages/grid.tsx
@@ -23,13 +23,13 @@ export default function Grid({
 
   return (
     <div
-      className="grid grid-cols-[repeat(7,1fr)] auto-rows-[1fr] gap-2.5 w-[500px] h-[500px] justify-center 
+      className="grid grid-cols-[repeat(7,1fr)] auto-rows-[1fr] gap-0 w-[500px] h-[500px] justify-center 
     absolute translate-x-[-30%] -translate-y-2/4 left-[30%] top-2/4"
     >
       {numbers.map((number) => (
         <div
           key={number}
-          className={`flex items-center justify-center rounded-[6%] number 
+          className={`flex items-center justify-center rounded-[1%] number border border-grey 
             ${
               isNumberSelected(number) ? 'selected ' + colorVariants[vacationType] : 'bg-mediumgrey'
             }`}
